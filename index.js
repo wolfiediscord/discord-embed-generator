676eb277dc94481b8fd5155656855388f4ac6473
Wolfie <45970663+wolfiediscord@users.noreply.github.com>
2020-06-13 00:49:33 -0500

    Update index.js
    Added checks for strings and updated errors.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+exports = () => {
+throw new Error("SyntaxError: Invalid usage of module 'discord-embed-generator'. Must include a method.")
+}
 exports.success = (msg) => {
-if(!msg) throw new Error('ReferenceError: A message wasn\'t defined on the function.')
+if(!msg) throw new Error('ReferenceError: Provided message was blank.')
+if(typeof msg !== 'string') throw new Error('TypeError: Message is not a string.');
     let embed = {
      description: `✅ ${msg}`,
      color: 0x2fa811
@@ -7,7 +11,8 @@ if(!msg) throw new Error('ReferenceError: A message wasn\'t defined on the funct
    return embed;
 }
 exports.warn = (msg) => {
-if(!msg) throw new Error('ReferenceError: A message wasn\'t defined on the function.')
+if(!msg) throw new Error('ReferenceError: Provided message was blank.')
+if(typeof msg !== 'string') throw new Error('TypeError: Message is not a string.');
     let embed = {
      description: `⚠️ ${msg}`,
      color: 0xfced1c
@@ -15,7 +20,8 @@ if(!msg) throw new Error('ReferenceError: A message wasn\'t defined on the funct
     return embed;
 }
 exports.info = (msg) => {
-if(!msg) throw new Error('ReferenceError: A message wasn\'t defined on the function.')
+if(!msg) throw new Error('ReferenceError: Provided message was blank.')
+if(typeof msg !== 'string') throw new Error('TypeError: Message is not a string.');
     let embed = {
     description: `ℹ️ ${msg}`,
     color: 0x3c8de7
@@ -23,7 +29,8 @@ if(!msg) throw new Error('ReferenceError: A message wasn\'t defined on the funct
     return embed;
 }
 exports.error = (msg) => {
-if(!msg) throw new Error('ReferenceError: A message wasn\'t defined on the function.')
+if(!msg) throw new Error('ReferenceError: Provided message was blank.')
+if(typeof msg !== 'string') throw new Error('TypeError: Message is not a string.');
     let embed = {
       description: `❌ ${msg}`,
       color: 0xff0000
